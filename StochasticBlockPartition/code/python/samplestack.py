38c905a41c0ce2dfa46ebb9ceda070ef8fe43e69
Frank Wanye <wanyef@mail.gvsu.edu>
2019-10-27 16:28:35 -0400

    Sample Stack now works with partitioned samples

--- StochasticBlockPartition/code/python/samplestack.py
+++ StochasticBlockPartition/code/python/samplestack.py
@@ -3,6 +3,8 @@
 from typing import Tuple, Dict, List
 import timeit
 
+import numpy as np
+
 from evaluation import Evaluation
 from graph import Graph
 from node_reassignment import fine_tune_membership
@@ -26,7 +28,7 @@ class SampleStack(object):
         self.t_load_start = timeit.default_timer()
         self.full_graph = Graph.load(args)
         self.t_load_end = timeit.default_timer()
-        self.stack = list()  # type: List[Tuple[Graph, Dict[int,int], Dict[int,int]]]
+        self.stack = list()  # type: List[Tuple[Graph, Sample]]
         self._sample(args)
         # self.stack.append((graph, None, None))
         self.t_sample_end = timeit.default_timer()
@@ -34,19 +36,20 @@ class SampleStack(object):
 
     def _sample(self, args):
         # Iteratively perform sampling
-        state = SampleState()
-        # graph = self.full_graph
-        for _ in range(args.sample_iterations):
+        for iteration in range(args.sample_iterations):
             # graph = self.stack[-1][0]
-            state, subgraph, vertex_mapping, block_mapping = self.full_graph.sample(args, state)
-            self.stack.append((subgraph, vertex_mapping, block_mapping))
+            if iteration == 0:
+                subgraph, sample = self.full_graph.sample(args)
+            else:
+                subgraph, sample = self.full_graph.sample(args, sample.state)
+            self.stack.append((subgraph, sample))
     # End of _sample()
 
     def _push(self):
         # Add a subsample to the stack
         raise NotImplementedError()
 
-    def _pop(self) -> Tuple[Graph, Dict, Dict]:
+    def _pop(self) -> Tuple[Graph, 'Sample']:
         # Propagate a subsample's results up the stack
         return self.stack.pop(0)
     # End of _pop()
@@ -77,29 +80,34 @@ class SampleStack(object):
                 the mapping of the communities/blocks from the combined sample to the full graph
         """
         # Propagate results back through the stack
-        subgraph, vertex_mapping, block_mapping = self._pop()
-        min_num_blocks = 0
+        subgraph, sample = self._pop()
+        min_num_blocks = -1
         denominator = 2
-        if args.sample_iterations > 1:
-            min_num_blocks = int(subgraph.num_nodes / denominator)
-            min_num_blocks = 0
-        subgraph_partition, evaluation = stochastic_block_partition(subgraph, args, subgraph_partition, evaluation,
+        # if args.sample_iterations > 1:
+        #     min_num_blocks = int(subgraph.num_nodes / denominator)
+        #     min_num_blocks = 0
+        combined_partition, evaluation = stochastic_block_partition(subgraph, args, subgraph_partition, evaluation,
                                                                     min_num_blocks)
+        combined_subgraph = subgraph
+        # print(np.unique(combined_partition.block_assignment))
         while len(self.stack) > 0:
+            subgraph, next_sample = self._pop()
+            sample_partition, evaluation = stochastic_block_partition(subgraph, args, None, evaluation,
+                                                                      min_num_blocks)
             t1 = timeit.default_timer()
-            subgraph, vertex_mapping, block_mapping = self._pop()
-            subgraph_partition = Partition.extend_sample(subgraph_partition.num_blocks, subgraph.out_neighbors,
-                                                         subgraph_partition.block_assignment, args)
+            combined_partition, combined_subgraph, sample = self.combine_partition_with_sample(
+                combined_partition, sample_partition, sample, next_sample, args
+            )
             t2 = timeit.default_timer()
-            denominator *= 2
-            min_num_blocks = int(subgraph_partition.num_blocks / denominator)
-            min_num_blocks = 0
-            if len(self.stack) == 0:
-                min_num_blocks = 0
-            subgraph_partition, evaluation = stochastic_block_partition(subgraph, args, subgraph_partition, evaluation,
-                                                                        min_num_blocks)
             evaluation.propagate_membership += (t2 - t1)
-        return subgraph, subgraph_partition, vertex_mapping, block_mapping, evaluation
+        print("=====Performing final (combined) sample partitioning=====")
+        if min_num_blocks > 0 or (args.sample_iterations > 1):
+            combined_partition.num_blocks_to_merge = 0
+            subgraph_partition, evaluation = stochastic_block_partition(combined_subgraph, args, combined_partition,
+                                                                        evaluation, min_num_blocks)
+        else:
+            subgraph_partition = combined_partition
+        return combined_subgraph, subgraph_partition, sample.vertex_mapping, sample.true_blocks_mapping, evaluation
     # End of unstack()
 
     def extrapolate_sample_partition(self, subgraph_partition: Partition, vertex_mapping: Dict[int,int], 
@@ -141,8 +149,26 @@ class SampleStack(object):
         return self.full_graph, full_graph_partition, evaluation
     # End of extrapolate_sample_partition()
 
-    def tail(self) -> Tuple[Graph, Partition, Dict]:
+    def tail(self) -> Tuple[Graph, 'Sample']:
         # Get innermost sample
         return self.stack[0]
     # End of tail()
+
+    def combine_partition_with_sample(self, combined_partition: Partition, sample_partition: Partition,
+        previous_sample: 'Sample', next_sample: 'Sample', args: 'argparse.Namespace') -> Tuple[Partition, Graph, 'Sample']:
+        """Combine the partition with the sample.
+        """
+        print("Adding sample partitioning results to previous partitioning results.")
+        # Combine vertice IDs
+        vertices = np.concatenate((list(previous_sample.vertex_mapping.keys()), list(next_sample.vertex_mapping.keys())))
+        # Combine true_block_mappings
+        block_ids_start = len(np.unique(combined_partition.block_assignment))
+        sample_block_assignment = np.asarray(sample_partition.block_assignment) + block_ids_start
+        block_assignment = np.concatenate((combined_partition.block_assignment, sample_block_assignment))
+        # Create a combined sample
+        subgraph, sample = self.full_graph.sample_from_vertex_ids(vertices, args)
+        # Create a combined partition
+        combined_partition = Partition(len(np.unique(block_assignment)), subgraph.out_neighbors, args, block_assignment)
+        return combined_partition, subgraph, sample
+    # End of combine_partition_with_sample()
 # End of SampleStack()
