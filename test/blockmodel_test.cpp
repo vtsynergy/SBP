be1d100f9fba4e198ad21abdbdf54354456b68d7
Frank Wanye <wanyef@vt.edu>
2022-12-14 00:09:29 -0500

    Added test for consecutive Blockmodel::block_merge calls

--- test/blockmodel_test.cpp
+++ test/blockmodel_test.cpp
@@ -94,7 +94,7 @@ TEST_F(BlockmodelTest, MergeBlockConsecutiveIsCorrect) {
 ////                       0  1  2  3  4  5  6  7  8  9  10
 ////        assignment = { 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2 };
     std::vector<int> new_assignment = { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 };
-    Blockmodel new_blockmodel(6, graph, 0.5, new_assignment);
+    Blockmodel new_blockmodel(3, graph, 0.5, new_assignment);
 //    int proposed_block = B.translate(1);
     Delta delta = block_merge::blockmodel_delta(0, 1, B);
     B.merge_block(0, 1, delta);
@@ -121,33 +121,6 @@ TEST_F(BlockmodelTest, MergeBlockConsecutiveIsCorrect) {
     EXPECT_EQ(B.degrees(row), new_blockmodel.degrees(row));
     }
     B.validate(graph);
-//    Delta delta = block_merge::blockmodel_delta(0, 1, B);
-//    B.merge_block(0, 1, delta);
-//    EXPECT_EQ(B.blockmatrix()->get(0, 0), 0);
-//    EXPECT_EQ(B.blockmatrix()->get(0, 1), 0);
-//    EXPECT_EQ(B.blockmatrix()->get(0, 2), 0);
-//    EXPECT_EQ(B.blockmatrix()->get(1, 0), 0);
-//    EXPECT_EQ(B.blockmatrix()->get(1, 1), 14);
-//    EXPECT_EQ(B.blockmatrix()->get(1, 2), 1);
-//    EXPECT_EQ(B.blockmatrix()->get(2, 0), 0);
-//    EXPECT_EQ(B.blockmatrix()->get(2, 1), 2);
-//    EXPECT_EQ(B.blockmatrix()->get(2, 2), 6);
-//    EXPECT_EQ(B.degrees_out(0), 0);
-//    EXPECT_EQ(B.degrees_out(1), 15);
-//    EXPECT_EQ(B.degrees_out(2), 8);
-//    EXPECT_EQ(B.degrees_in(0), 0);
-//    EXPECT_EQ(B.degrees_in(1), 16);
-//    EXPECT_EQ(B.degrees_in(2), 7);
-//    EXPECT_EQ(B.degrees(0), 0);
-//    EXPECT_EQ(B.degrees(1), 17);
-//    EXPECT_EQ(B.degrees(2), 9);
-//    for (int i = 0; i < B.block_assignment().size(); ++i) {
-//        if (i < 7) {
-//            EXPECT_EQ(B.block_assignment(i), 1);
-//        } else {
-//            EXPECT_EQ(B.block_assignment(i), 2);
-//        }
-//    }
 }
 
 TEST_F(BlockmodelTest, MoveVertexWithDenseEdgeCountUpdatesIsCorrect) {
@@ -278,68 +251,58 @@ TEST_F(BlockmodelComplexTest, MergeBlockIsCorrect) {
     B.validate(graph);
 }
 
-//TEST_F(BlockmodelComplexTest, MergeBlockConsecutiveIsCorrect) {
-//    // TODO
-//    //  _ | 0 | 1 | 2 | 3 | 4 | 5 before
-//    //  0 | 4   1   0   0   0   0 | 5
-//    //  1 | 2   0   0   1   0   1 | 4
-//    //  2 | 1   0   0   1   0   0 | 2
-//    //  3 | 0   0   2   2   1   0 | 5
-//    //  4 | 0   2   0   0   0   0 | 2
-//    //  5 | 0   0   0   1   2   2 | 5
-//    //      7   3   2   5   3   3 | 23
-//    //  _ | 0 | 1 | 2 | 3 | 4 | 5 after
-//    //  0 | 4   1   0   0   0   0 | 5
-//    //  1 | 2   0   0   1   0   1 | 4
-//    //  2 | 1   0   0   1   0   0 | 2
-//    //  3 | 0   0   2   2   1   0 | 5
-//    //  4 | 0   2   0   0   0   0 | 2
-//    //  5 | 0   0   0   1   2   2 | 5
-//    //      7   3   2   5   3   3 | 23
-//    std::vector<int> new_assignment = { 1, 1, 1, 1, 2, 3, 3, 4, 5, 1, 5 };
-//    Blockmodel new_blockmodel(6, graph, 0.5, new_assignment);
-//    Delta delta = block_merge::blockmodel_delta(0, 1, B);
-//    B.merge_block(0, 1, delta);
-//    for (int row = 0; row < B.getNum_blocks(); ++row) {
-//        for (int col = 0; col < B.getNum_blocks(); ++col) {
-//            int val1 = B.blockmatrix()->get(row, col);
-//            int val2 = new_blockmodel.blockmatrix()->get(row, col);
-//            EXPECT_EQ(val1, val2)
-//                                << "Blockmatrices differ at " << row << "," << col << " : using updates, value = " << val1
-//                                << " using assignment, value = " << val2;
-//        }
-//        EXPECT_EQ(B.block_assignment(row), new_blockmodel.block_assignment(row));
-//        EXPECT_EQ(B.degrees_out(row), new_blockmodel.degrees_out(row));
-//        EXPECT_EQ(B.degrees_in(row), new_blockmodel.degrees_in(row));
-//        EXPECT_EQ(B.degrees(row), new_blockmodel.degrees(row));
-//    }
-//    B.validate(graph);
-////    EXPECT_EQ(B.blockmatrix()->get(0, 0), 0);
-////    EXPECT_EQ(B.blockmatrix()->get(0, 1), 0);
-////    EXPECT_EQ(B.blockmatrix()->get(0, 2), 0);
-////    EXPECT_EQ(B.blockmatrix()->get(1, 0), 0);
-////    EXPECT_EQ(B.blockmatrix()->get(1, 1), 14);
-////    EXPECT_EQ(B.blockmatrix()->get(1, 2), 1);
-////    EXPECT_EQ(B.blockmatrix()->get(2, 0), 0);
-////    EXPECT_EQ(B.blockmatrix()->get(2, 1), 2);
-////    EXPECT_EQ(B.blockmatrix()->get(2, 2), 6);
-////    EXPECT_EQ(B.degrees_out(0), 0);
-////    EXPECT_EQ(B.degrees_out(1), 15);
-////    EXPECT_EQ(B.degrees_out(2), 8);
-////    EXPECT_EQ(B.degrees_in(0), 0);
-////    EXPECT_EQ(B.degrees_in(1), 16);
-////    EXPECT_EQ(B.degrees_in(2), 7);
-////    EXPECT_EQ(B.degrees(0), 0);
-////    EXPECT_EQ(B.degrees(1), 17);
-////    EXPECT_EQ(B.degrees(2), 9);
-////    for (int i = 0; i < B.block_assignment().size(); ++i) {
-////        if (i < 7) {
-////            EXPECT_EQ(B.block_assignment(i), 1);
-////        } else {
-////            EXPECT_EQ(B.block_assignment(i), 2);
-////        }
-////    }
-//}
+TEST_F(BlockmodelComplexTest, MergeBlockConsecutiveIsCorrect) {
+    // TODO
+    //  _ | 0 | 1 | 2 | 3 | 4 | 5 before
+    //  0 | 4   1   0   0   0   0 | 5
+    //  1 | 2   0   0   1   0   1 | 4
+    //  2 | 1   0   0   1   0   0 | 2
+    //  3 | 0   0   2   2   1   0 | 5
+    //  4 | 0   2   0   0   0   0 | 2
+    //  5 | 0   0   0   1   2   2 | 5
+    //      7   3   2   5   3   3 | 23
+    //  _ | 0 | 1 | 2 | 3 | 4 | 5 after
+    //  0 | 4   1   0   0   0   0 | 5
+    //  1 | 2   0   0   1   0   1 | 4
+    //  2 | 1   0   0   1   0   0 | 2
+    //  3 | 0   0   2   2   1   0 | 5
+    //  4 | 0   2   0   0   0   0 | 2
+    //  5 | 0   0   0   1   2   2 | 5
+    //      7   3   2   5   3   3 | 23
+    std::vector<int> new_assignment = { 4, 4, 4, 4, 4, 3, 3, 4, 5, 4, 5 };
+    Blockmodel new_blockmodel(6, graph, 0.5, new_assignment);
+//    int proposed_block = B.translate(1);
+    Delta delta = block_merge::blockmodel_delta(0, 1, B);
+    B.merge_block(0, 1, delta);
+    std::cout << "After first merge: " << std::endl;
+    B.print_blockmodel();
+//    proposed_block = B.translate(0);
+//    std::cout << "translated block = " << proposed_block << std::endl;
+    delta = block_merge::blockmodel_delta(2, 4, B);
+    std::cout << "computed second delta" << std::endl;
+    B.merge_block(2, 4, delta);
+    std::cout << "After second merge: " << std::endl;
+    B.print_blockmodel();
+    delta = block_merge::blockmodel_delta(1, 2, B);
+    std::cout << "computed third delta" << std::endl;
+    B.merge_block(1, 2, delta);
+    std::cout << "After third merge: " << std::endl;
+    B.print_blockmodel();
+    for (int row = 0; row < B.getNum_blocks(); ++row) {
+        for (int col = 0; col < B.getNum_blocks(); ++col) {
+            int val1 = B.blockmatrix()->get(row, col);
+            int val2 = new_blockmodel.blockmatrix()->get(row, col);
+            EXPECT_EQ(val1, val2)
+                                << "Blockmatrices differ at " << row << "," << col << " : using updates, value = " << val1
+                                << " using assignment, value = " << val2;
+        }
+        EXPECT_EQ(B.block_assignment(row), new_blockmodel.block_assignment(row));
+        EXPECT_EQ(B.degrees_out(row), new_blockmodel.degrees_out(row));
+        EXPECT_EQ(B.degrees_in(row), new_blockmodel.degrees_in(row));
+        EXPECT_EQ(B.degrees(row), new_blockmodel.degrees(row));
+    }
+    B.validate(graph);
+}
 
 TEST_F(BlockmodelComplexTest, MoveVertexWithDenseEdgeCountUpdatesIsCorrect) {
     B.move_vertex(6, 3, Proposal.proposal, Updates, new_block_degrees.block_degrees_out, new_block_degrees.block_degrees_in, new_block_degrees.block_degrees);
