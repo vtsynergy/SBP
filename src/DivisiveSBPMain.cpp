f9ec79b7d3f25ee2ed273a88d059d86956f1505a
Frank Wanye <wanyef@vt.edu>
2024-09-19 16:37:32 -0400

    Added sampling to DivisiveSBP

--- src/DivisiveSBPMain.cpp
+++ src/DivisiveSBPMain.cpp
@@ -10,6 +10,8 @@
 #include "args.hpp"
 #include "blockmodel/blockmodel.hpp"
 #include "distributed/dist_divisive_sbp.hpp"
+#include "distributed/dist_finetune.hpp"
+#include "divisive_sbp.hpp"
 #include "entropy.hpp"
 #include "evaluate.hpp"
 #include "finetune.hpp"
@@ -17,8 +19,7 @@
 #include "globals.hpp"
 #include "mpi_data.hpp"
 #include "partition.hpp"
-//#include "sample.hpp"
-#include "divisive_sbp.hpp"
+#include "sample.hpp"
 #include "utils.hpp"
 
 //double sample_time = 0.0;
@@ -75,7 +76,50 @@ int main(int argc, char* argv[]) {
     Partition partition;
     partition.graph = Graph::load();
     double start = MPI_Wtime();
-    run(partition);
+    if (args.samplesize <= 0.0) {
+        double sample_start_t = MPI_Wtime();
+        std::cout << "Running sampling with size: " << args.samplesize << std::endl;
+        sample::Sample s = sample::sample(partition.graph);
+        if (mpi.num_processes > 1) {
+            MPI_Bcast(s.mapping.data(), (int) partition.graph.num_vertices(), MPI_LONG, 0, mpi.comm);
+            if (mpi.rank > 0) {
+                std::vector<long> vertices;
+                for (const long &mapped_id : s.mapping) {
+                    if (mapped_id >= 0)
+                        vertices.push_back(mapped_id);
+                }
+                s = sample::from_vertices(partition.graph, vertices, s.mapping);
+            }
+            MPI_Barrier(mpi.comm);
+        }
+        Partition sample_partition;
+        sample_partition.graph = std::move(s.graph);  // s.graph may be empty now
+        // add timer
+        double sample_end_t = MPI_Wtime();
+        timers::sample_time = sample_end_t - sample_start_t;
+        run(sample_partition);
+        double extend_start_t = MPI_Wtime();
+        s.graph = std::move(sample_partition.graph);  // refill s.graph
+        // extend sample to full graph
+        // TODO: this seems deterministic...
+        std::vector<long> assignment = sample::extend(partition.graph, sample_partition.blockmodel, s);
+        // fine-tune full graph
+        double finetune_start_t = MPI_Wtime();
+        if (mpi.num_processes > 1) {
+            Rank_indices = std::vector<long>();  // reset the rank_indices
+            auto blockmodel = TwoHopBlockmodel(sample_partition.blockmodel.num_blocks(), partition.graph, 0.5, assignment);
+            partition.blockmodel = finetune::dist::finetune_assignment(blockmodel, partition.graph);
+        } else {
+            partition.blockmodel = Blockmodel(sample_partition.blockmodel.num_blocks(), partition.graph, 0.5, assignment);
+            partition.blockmodel = finetune::finetune_assignment(partition.blockmodel, partition.graph);
+        }
+        double finetune_end_t = MPI_Wtime();
+        timers::sample_extend_time = finetune_start_t - extend_start_t;
+        timers::sample_finetune_time = finetune_end_t - finetune_start_t;
+    } else {
+        std::cout << "Running without sampling." << std::endl;
+        run(partition);
+    }
     // evaluate
     double end = MPI_Wtime();
     evaluate_partition(partition.graph, partition.blockmodel, end - start);
